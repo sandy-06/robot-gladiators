df77ff01c928a511b518702f43310012e2c9cb6d
sandy <sandy-sanders@msn.com>
2021-09-17 21:27:53 -0700

    lost a /

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 window.prompt("what is your robot's name?");
 
-/this creates a function named "fight"
+//this creates a function named "fight"
 function fight() {
     window.alert("The fight has begun!");
 }
