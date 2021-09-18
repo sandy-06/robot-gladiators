080322e50b0a2cec76c0d2b4a27ea6d40e9261f3
sandy <sandy-sanders@msn.com>
2021-09-18 11:51:16 -0700

    robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,15 @@
-window.prompt("what is your robot's name?");
-
+var playerName = window.prompt("what is your robot's name?");
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+console.log("this is a string, good for leaving yourself a message");
+//this will do mathe and log 20
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
 //this creates a function named "fight"
 function fight() {
     window.alert("The fight has begun!");
 }
-fight();
+//fight();
 
 
 
